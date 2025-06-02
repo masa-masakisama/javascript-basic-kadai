1391f5b38d63b02a19151659f4af993457657136
masa-masakisama <mannpukukabigonn@outlook.jp>
2025-06-02 11:21:35 +0900

    再再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,20 +1,11 @@
-// 「年」の関数を定義する
-const onYear = new Date("June 02, 2025 10:55:30");
+// 今日の日付を取得する
+const today = new Date();
 
-// 今日の「年」を設定する
-onYear.setFullYear(2025);
+const year = today.getFullYear();
 
-// 「月」の関数を定義する
-const onMonth = new Date("June 02, 2025 10:55:30");
+const month = today.getMonth() + 1;
 
-// 今日の「月」を設定する
-onMonth.setMonth(06) + 1;
-
-// 「日」の関数を定義する
-const onDate = new Date("June 02, 2025 10:55:30");
-
-// 今日の「日」を設定する
-onDate.setDate(02);
+const day = today.getDate();
 
 // 今日の日付を出力する
-console.log(onYear.getFullYear() + '年' + onMonth.getMonth() + '月' + onDate.getDate() + '日');
+console.log(year + '年' + month + '月' + day + '日');
