11aef2f098244d9b6a687e1fd937572640db572c
masa-masakisama <mannpukukabigonn@outlook.jp>
2025-06-04 09:21:49 +0900

    修正

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,10 +1,10 @@
 // btnというidをもつHTML要素を取得し、定数に代入する
 const changeBtn = document.getElementById('btn');
+ // textというidをもつHTML要素を取得し、定数に代入する
+  const changeText = document.getElementById('text');
 
 // HTML要素がクリックされたときにイベント処理を実行する
 changeBtn.addEventListener('click', () => {
-  // textというidをもつHTML要素を取得し、定数に代入する
-  const changeText = document.getElementById('text');
   // HTML要素がクリックされたとき、chageTextに「ボタンをクリックしました」というテキストを再代入する
   changeText.textContent = 'ボタンをクリックしました';
 })
